2fa70d7a4fdb2be93fd9d20c142731b7e26dab77
tankai215@hotmail.com <>
2021-08-11 15:54:06 +0800

    Auto save at 08/11/2021 03:54 下午

--- Assets/Scripts/GeneralEffect/PostEffectBasic.cs
+++ Assets/Scripts/GeneralEffect/PostEffectBasic.cs
@@ -38,9 +38,16 @@ public class PostEffectBasic : MonoBehaviour
     // 后处理入口函数
     protected virtual void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        
+        // source 参数：是场景渲染后的图像渲染纹理
+        // destination 参数：是经过我们的后处理后要填充的目标图像渲染纹理
+        // if (!IsSupported) { Graphics.Blit(source, destination); return; } // 不支持
+        // mat.SetFloat("paramName", xx);
+        // mat.SetColor("paramName", xx);
+        // Graphics.Blit(source, destination, mat);
+        //if (!IsSupported) { Graphics.Blit(source, destination); return; } // 不支持
         mat.SetFloat("_PixelInterval", PixelInterval);
         mat.SetFloat("_PixelItensity", PixelItensity);
+        //mat.SetColor("_EdgeColor", edgeColor);
         Graphics.Blit(source, destination, mat);
     }
 }
