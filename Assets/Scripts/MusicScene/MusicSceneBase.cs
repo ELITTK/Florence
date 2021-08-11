32080b3cf6936f07348a3f10021f08a00290ddd0
tankai215@hotmail.com <>
2021-08-11 16:36:26 +0800

    Auto save at 08/11/2021 04:36 下午

--- Assets/Scripts/MusicScene/MusicSceneBase.cs
+++ Assets/Scripts/MusicScene/MusicSceneBase.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class MusicSceneBase : MonoBehaviour
 {
+    public int bpm;
+    public float introTime;
+
+    private float noteTime, timer;
+    private bool isIntroed = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        noteTime = 3600 / bpm;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        NoteCtrol();
+    }
+
+    void NoteCtrol()
+    {
+        timer += Time.deltaTime;
+        if (isIntroed)  
+        {
+            //开头已完成
+
+        }
+        else 
+        {
+            if (timer > introTime)
+            {
+                isIntroed = true;
+                timer -= introTime;
+            }
+        }
     }
 }
