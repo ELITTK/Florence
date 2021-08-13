222313d583cbb2df504553d0c23f9ba5e63060c7
tankai215@hotmail.com <>
2021-08-13 11:43:11 +0800

    Auto save at 08/13/2021 11:43 上午

--- Assets/Scripts/MusicScene/TrackControl.cs
+++ Assets/Scripts/MusicScene/TrackControl.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class TrackControl : MonoBehaviour
 {
     public GameObject[] tracks = new GameObject[3];
+    public int speedBase, speedCtrl;
+
+    private float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Transform playerPosi = GameObject.FindGameObjectWithTag("Player").transform;
+        float length = Mathf.Abs(transform.position.x - playerPosi.position.x);
+        speed = length / speedBase * speedCtrl;
+
     }
 
     // Update is called once per frame
